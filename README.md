f9969312a03794c2e371602a5dd9a9180b15c1e0
mengxinayan <yanhou529@gmail.com>
2020-02-03 08:40:11 +0800

    Add #118 Pascal's Triangle

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ If you have any questions or advices, please discuss them in [Issues](https://gi
 ### Algorithm
 | # | Title | Solution | Difficulty | Related Topics |
 | :-: | :-: | :-: | :-: | :-: |
+| 118 | [Pascal's Triangle](https://leetcode.com/problems/pascals-triangle/) | [Python3](./algorithm/python/118_pascal's_triangle.py) | Easy | Array |
 | 112 | [Path Sum](https://leetcode.com/problems/path-sum/) | [Python3](./algorithm/python/112_path_sum.py) | Easy | Tree, Depth-first Search |
 | 111 | [Minimum Depth of Binary Tree](https://leetcode.com/problems/minimum-depth-of-binary-tree/) | [Python3](./algorithm/python/111_minimum_depth_of_binary_tree.py) | Easy | Tree, Depth-first Search, Breadth-first Search |
 | 110 | [Balanced Binary Tree](https://leetcode.com/problems/balanced-binary-tree/) | [Python3](./algorithm/python/110_balanced_binary_tree.py) | Easy | Tree, Depth-first Search |
---
+++ algorithm/python/118_pascal's_triangle.py
@@ -0,0 +1,39 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        
+        def cal_next(nums: List[int]) -> List[int]:
+            res = [0 for i in range( len(nums)+1 )]
+            res[0] = 1
+            res[-1] = 1
+            for i in range(1, len(nums)):
+                res[i] = nums[i] + nums[i-1]
+            return res
+
+        if numRows == 0:
+            return []
+        elif numRows == 1:
+            return [[1]]
+        else:
+            res = [[1], [1,1]]
+            for i in range(numRows-2):
+                tmp = cal_next(res[-1])
+                res.append(tmp)
+            return res
+'''
+    This is my personal record of solving Leetcode Problems. 
+    If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+    Copyright (C) 2020  mengxinayan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
