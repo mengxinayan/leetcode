cfea1d1f5f280425f37cce278da156a2df020a29
mengxinayan <yanhou529@gmail.com>
2020-02-27 21:31:34 +0800

    Add #349 Intersection of Two Arrays

--- README-zh.md
+++ README-zh.md
@@ -13,6 +13,7 @@
 
 | # | 题名 | 解答 | 难度 | 相关标签 |
 | :-: | :-: | :-: | :-: | :-: |
+| 349 | [Intersection of Two Arrays](https://leetcode-cn.com/problems/intersection-of-two-arrays/) | [Python3](./algorithm/python/349_intersection_of_two_arrays.py) | Easy | Sort, Hash Table, Two Pointers, Binary Search |
 | 345 | [Reverse Vowels of a String](https://leetcode-cn.com/problems/reverse-vowels-of-a-string/) | [Python3](./algorithm/python/345_reverse_vowels_of_a_string.py) | Easy | Two Pointers, String |
 | 344 | [Reverse String](https://leetcode-cn.com/problems/reverse-string/) | [Python3](./algorithm/python/344_reverse_string.py) | Easy | Two Pointers, String |
 | 342 | [Power of Four](https://leetcode-cn.com/problems/power-of-four/) | [Python3](./algorithm/python/342_power_of_four.py) | Easy | Bit Manipulation |
---
+++ algorithm/python/349_intersection_of_two_arrays.py
@@ -0,0 +1,36 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        # len(nums1) < len(set2)
+        def cal(nums1: List[int], set2: set) -> List[int]:
+            res = []
+            for i in range(len(nums1)):
+                if nums1[i] in set2:
+                    res.append(nums1[i])
+            return res
+        
+        set1 = set(nums1)
+        set2 = set(nums2)
+        if len(set1) <= len(set2):
+            return cal(list(set1), set2)
+        else:
+            return cal(list(set2), set1)
+
+'''
+    This is my personal record of solving Leetcode Problems. 
+    If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+    Copyright (C) 2020  mengxinayan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
