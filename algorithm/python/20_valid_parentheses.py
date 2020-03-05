418ec93686136eea8d86b528324b59ab779aae6c
mengxinayan <yanhou529@gmail.com>
2020-03-05 09:15:37 +0800

    Update #20 Valid Parentheses

--- algorithm/python/20_valid_parentheses.py
+++ algorithm/python/20_valid_parentheses.py
@@ -31,6 +31,7 @@ class Solution:
         if len(chs) == 0:
             res = True
         return res
+
 '''
     This is my personal record of solving Leetcode Problems. 
     If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
