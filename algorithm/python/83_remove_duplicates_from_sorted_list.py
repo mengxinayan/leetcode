af24aee477a6d1ee74834d112d9a6f054249e1c0
mengxinayan <yanhou529@gmail.com>
2020-03-08 20:43:06 +0800

    Update #83 Remove Duplicates from Sorted List

--- algorithm/python/83_remove_duplicates_from_sorted_list.py
+++ algorithm/python/83_remove_duplicates_from_sorted_list.py
@@ -20,6 +20,7 @@ class Solution:
                 p = p.next
                 q = q.next
         return head
+
 '''
     This is my personal record of solving Leetcode Problems. 
     If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
