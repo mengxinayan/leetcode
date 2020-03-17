fc4df7f4f380a7a57cf4c46623254400e54167b8
mengxinayan <yanhou529@gmail.com>
2020-03-17 20:16:49 +0800

    Add #485 Max Consecutive Ones

---
+++ algorithm/python/485_max_consecutive_ones.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        max_times = 0
+        one_times = 0
+        for number in nums:
+            if number == 1:
+                one_times += 1
+            else:
+                max_times = max(max_times, one_times)
+                one_times = 0
+        max_times = max(max_times, one_times)
+        return max_times
+
+'''
+    This is my personal record of solving Leetcode Problems. 
+    If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+    Copyright (C) 2020  mengxinayan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
