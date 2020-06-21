a46e6753128363a992c0f49dc9a91e8f99d691d9
mengxinayan <yanhou529@gmail.com>
2020-06-21 16:17:06 +0800

    Add #538 Convert BST to Greater Tree

---
+++ algorithm/python/538_convert_BST_to_greater_tree.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        def recursive(root: TreeNode):
+            if root != None:
+                recursive(root.right)
+                root.val = root.val + self.num_sum
+                self.num_sum = root.val
+                recursive(root.left)
+            else:
+                pass
+
+        self.num_sum = 0
+        recursive(root)
+        return root
+
+'''
+    This is my personal record of solving Leetcode Problems. 
+    If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+    Copyright (C) 2020  mengxinayan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
