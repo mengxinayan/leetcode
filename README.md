a9649afb13c0f73c9ed41139da4f082a6d7dc5fd
mengxinayan <yanhou529@gmail.com>
2020-08-02 16:57:24 +0800

    Add #605 Can Place Flowers

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ If you have any questions or advices, please discuss them in [Issues](https://gi
 ### Algorithm
 | # | Title | Solution | Difficulty | Related Topics |
 | :-: | :-: | :-: | :-: | :-: |
+| 605 | [Can Place Flowers](https://leetcode.com/problems/can-place-flowers/) | [Python3](./algorithm/python/605_can_place_flowers.py) | Easy | Array |
 | 599 | [Minimum Index Sum of Two Lists](https://leetcode.com/problems/minimum-index-sum-of-two-lists/) | [Python3](./algorithm/python/599_minimum_index_sum_of_two_lists.py) | Easy | Hash Table |
 | 598 | [Range Addition II](https://leetcode.com/problems/range-addition-ii/) | [Python3](./algorithm/python/598_range_addition_II.py) | Easy | Math |
 | 594 | [Longest Harmonious Subsequence](https://leetcode.com/problems/longest-harmonious-subsequence/) | [Python3](./algorithm/python/594_longest_harmonious_subsequence.py) | Easy | Hash Table |
---
+++ algorithm/python/605_can_place_flowers.py
@@ -0,0 +1,47 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        if len(flowerbed) <= 2:
+            if (sum(flowerbed) + n) <= 1:
+                return True
+            else:
+                return False
+        else:
+            count = i = 0
+            while i < len(flowerbed):
+                if flowerbed[i] == 0:
+                    if i == 0:
+                        if flowerbed[i+1] == 0:
+                            flowerbed[i] = 1
+                            count += 1
+                    elif i == len(flowerbed)-1:
+                        if (flowerbed[i-1] == 0):
+                            flowerbed[i] = 1
+                            count += 1
+                    else:
+                        if (flowerbed[i-1] == 0) and (flowerbed[i+1] == 0):
+                            flowerbed[i] = 1
+                            count += 1
+                i += 1
+            if count >= n:
+                return True
+            else:
+                return False
+
+'''
+    This is my personal record of solving Leetcode Problems. 
+    If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+    Copyright (C) 2020  mengxinayan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
