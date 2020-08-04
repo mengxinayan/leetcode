e56b6d59436002b2c99472edff472412a97fd017
mengxinayan <yanhou529@gmail.com>
2020-08-04 10:58:53 +0800

    Update #620 Not Boring Movies

--- database/MySQL/620_not_boring_movies.sql
+++ database/MySQL/620_not_boring_movies.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 SELECT id, movie, description, rating
 FROM cinema
-WHERE id % 2 = 1 and description <> 'boring'
+WHERE (id % 2 = 1) and (description <> 'boring')
 ORDER BY rating DESC
 
 /*
