827490531d117f5ad9f3549d6124b2471b2235af
mengxinayan <yanhou529@gmail.com>
2020-09-13 20:41:34 +0800

    Add #771 Jewels and Stones

--- README-zh.md
+++ README-zh.md
@@ -13,6 +13,7 @@
 
 | # | 题名 | 解答 | 难度 | 相关标签 |
 | :-: | :-: | :-: | :-: | :-: |
+| 771 | [Jewels and Stones](https://leetcode-cn.com/problems/jewels-and-stones/) | [Python3](./algorithm/python/771_jewels_and_stones.py) | Easy | Hash Table |
 | 766 | [Toeplitz Matrix](https://leetcode-cn.com/problems/toeplitz-matrix/) | [Python3](./algorithm/python/766_toeplitz_matrix.py) | Easy | Array |
 | 762 | [Prime Number of Set Bits in Binary Representation](https://leetcode-cn.com/problems/prime-number-of-set-bits-in-binary-representation/) | [Python3](./algorithm/python/762_prime_number_of_set_bits_in_binary_representation.py) | Easy | Bit Manipulation |
 | 748 | [Shortest Completing Word](https://leetcode-cn.com/problems/shortest-completing-word/) | [Python3](./algorithm/python/748_shortest_completing_word.py) | Easy | Hash Table |
---
+++ algorithm/python/771_jewels_and_stones.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        stone_dict = {}
+        for ch in S:
+            if ch not in stone_dict:
+                stone_dict[ch] = 1
+            else:
+                stone_dict[ch] += 1
+        res = 0
+        for ch in J:
+            if ch in stone_dict:
+                res += stone_dict[ch]
+        return res
+
+'''
+    This is my personal record of solving Leetcode Problems. 
+    If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+    Copyright (C) 2020  mengxinayan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
