9803840beb0186751dbe18f30a28f590b72bc82d
mengxinyan <yanhou529@gmail.com>
2020-09-27 00:46:34 -0400

    Update #836 Rectangle Overlap

--- algorithm/python/836_rectangle_overlap.py
+++ algorithm/python/836_rectangle_overlap.py
@@ -20,4 +20,5 @@ class Solution:
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    alon
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
