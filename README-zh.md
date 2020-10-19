9d9a5b9bf3c4da3599565b3d932beb277bb6ece0
mengxinyan <yanhou529@gmail.com>
2020-10-18 20:43:08 -0400

    Add #914 X of a Kind in a Deck of Cards

--- README-zh.md
+++ README-zh.md
@@ -13,6 +13,7 @@
 
 | # | 题名 | 解答 | 难度 | 相关标签 |
 | :-: | :-: | :-: | :-: | :-: |
+| 914 | [X of a Kind in a Deck of Cards](https://leetcode-cn.com/problems/x-of-a-kind-in-a-deck-of-cards/) | [Python3](./algorithm/python/914_x_of_a_kind_in_a_deck_of_cards.py) | Easy | Array, Math |
 | 908 | [Smallest Range I](https://leetcode-cn.com/problems/smallest-range-i/) | [Python3](./algorithm/python/908_smallest_range_I.py) | Easy | Math |
 | 905 | [Sort Array by Parity](https://leetcode-cn.com/problems/sort-array-by-parity/) | [Python3](./algorithm/python/905_sort_array_by_parity.py) | Easy | Array |
 | 897 | [Increasing Order Search Tree](https://leetcode-cn.com/problems/increasing-order-search-tree/) | [Python3](./algorithm/python/897_increasing_order_search_tree.py) | Easy | Tree, Depth-first Search |
---
+++ algorithm/python/914_x_of_a_kind_in_a_deck_of_cards.py
@@ -0,0 +1,57 @@
+class Solution:
+    def hasGroupsSizeX(self, deck: List[int]) -> bool:
+        '''
+        NOTE: Allow two sets of cards to have the same number.
+        For example: deck = [1,1,1,1,2,2], Possible partition [1,1],[1,1],[2,2]
+        
+        允许有两组卡牌，他们的数字是相同的。
+        比如：deck = [1,1,1,1,2,2], 分组情况 [1,1],[1,1],[2,2]
+        '''
+        
+        def cal_gcd(num1: int, num2: int) -> int:
+            tmp = 0
+            while num2 != 0:
+                tmp = num1 % num2
+                num1 = num2
+                num2 = tmp
+            return num1
+
+        num_times = {}
+        for num in deck:
+            if num in num_times:
+                num_times[num] += 1
+            else:
+                num_times[num] = 1
+        times = list(num_times.values())
+        if len(times) == 1:
+            if times[0] >= 2:
+                return True
+            else:
+                return False
+        else:
+            arr_gcd = cal_gcd(times[0], times[1])
+            for i in range(2, len(times)):
+                arr_gcd = cal_gcd(arr_gcd, times[i])
+            if arr_gcd >= 2:
+                return True
+            else:
+                return False
+
+'''
+    This is my personal record of solving Leetcode Problems. 
+    If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+    Copyright (C) 2020  mengxinayan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
