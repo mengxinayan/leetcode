512c535f3f1ff0f385e08f432d881875a0c16e7d
mengxinyan <yanhou529@gmail.com>
2020-12-25 05:07:28 -0500

    Add #1380 Lucky Numbers in a Matrix

--- README-zh.md
+++ README-zh.md
@@ -13,6 +13,7 @@
 
 | # | 题名 | 解答 | 难度 | 相关标签 |
 | :-: | :-: | :-: | :-: | :-: |
+| 1380 | [Lucky Numbers in a Matrix](https://leetcode-cn.com/problems/lucky-numbers-in-a-matrix/) | [Python3](./algorithm/python/1380_lucky_numbers_in_a_matrix.py) | Easy | Array |
 | 1374 | [Generate a String With Characters That Have Odd Counts](https://leetcode-cn.com/problems/generate-a-string-with-characters-that-have-odd-counts/) | [Python3](./algorithm/python/1374_generate_a_string_with_characters_that_have_odd_counts.py) | Easy | String |
 | 1370 | [Increasing Decreasing String](https://leetcode-cn.com/problems/increasing-decreasing-string/) | [Python3](./algorithm/python/1370_increasing_decreasing_string.py) | Easy | Sort, String |
 | 1365 | [How Many Numbers Are Smaller Than the Current Number](https://leetcode-cn.com/problems/how-many-numbers-are-smaller-than-the-current-number/) | [Python3](./algorithm/python/1365_how_many_numbers_are_smaller_than_the_current_number.py) | Easy | Array, Hash Table |
---
+++ algorithm/python/1380_lucky_numbers_in_a_matrix.py
@@ -0,0 +1,34 @@
+class Solution:
+    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+        row_min = []
+        for row in matrix:
+            row_min.append(min(row))
+        col_max = []
+        for i in range(len(matrix[0])):
+            col = [matrix[j][i] for j in range(len(matrix))]
+            col_max.append(max(col))
+        res = []
+        for i in range(len(row_min)):
+            for j in range(len(col_max)):
+                if row_min[i] == col_max[j]:
+                    res.append(matrix[i][j])
+        return res
+
+'''
+    This is my personal record of solving Leetcode Problems. 
+    If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+    Copyright (C) 2020  mengxinayan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
