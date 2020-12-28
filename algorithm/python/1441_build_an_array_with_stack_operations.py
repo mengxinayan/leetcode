ac1e14b7db1cd44b88dc79f0ef9d8dbaa9365310
mengxinyan <yanhou529@gmail.com>
2020-12-28 03:20:11 -0500

    Add #1441 Build an Array With Stack Operations

---
+++ algorithm/python/1441_build_an_array_with_stack_operations.py
@@ -0,0 +1,33 @@
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        target_set = set(target)
+        res = []
+        push = 'Push'
+        pop = 'Pop'
+        length = min(n+1, max(target)+1)
+        for i in range(1, length):
+            if i in target_set:
+                res.append(push)
+            else:
+                res.append(push)
+                res.append(pop)
+        return res
+
+'''
+    This is my personal record of solving Leetcode Problems. 
+    If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+    Copyright (C) 2020  mengxinayan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
