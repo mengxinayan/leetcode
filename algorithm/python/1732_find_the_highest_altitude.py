d60a427f1f9c486c4f46750b5cbf75118acdbf3e
mengxinyan <yanhou529@gmail.com>
2021-01-26 20:16:23 -0500

    Add #1732 Find the Highest Altitude

---
+++ algorithm/python/1732_find_the_highest_altitude.py
@@ -0,0 +1,38 @@
+# Solution 1
+
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        altitudes = [0]
+        for i in range(len(gain)):
+            altitudes.append(altitudes[i]+gain[i])
+        return max(altitudes)
+
+
+# Solution 2
+
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        max_altitude = curr_altitude = 0
+        for num in gain:
+            curr_altitude += num
+            max_altitude = max(max_altitude, curr_altitude)
+        return max_altitude
+
+'''
+    This is my personal record of solving Leetcode Problems. 
+    If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+    Copyright (C) 2020  mengxinayan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
