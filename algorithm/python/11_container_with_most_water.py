3f1bdf3713b1a9e1f19ae2f12d824c1a93db3f5f
mengxinyan <yanhou529@gmail.com>
2021-02-02 20:43:55 -0500

    Add #11 Container With Most Water

---
+++ algorithm/python/11_container_with_most_water.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        max_water = -1
+        while left < right:
+            max_water = max(max_water, min(height[left], height[right]) * (right - left))
+            if height[left] <= height[right]:
+                left += 1
+            else:
+                right -= 1
+        return max_water
+
+'''
+    This is my personal record of solving Leetcode Problems. 
+    If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+    Copyright (C) 2020  mengxinayan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
