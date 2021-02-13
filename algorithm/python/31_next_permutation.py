9a44607abf5dfa9221168bbfa223159b8df2df71
mengxinyan <yanhou529@gmail.com>
2021-02-12 21:24:49 -0500

    Add #31 Next Permutation

---
+++ algorithm/python/31_next_permutation.py
@@ -0,0 +1,44 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = len(nums) - 2
+        right = len(nums) - 1
+        while left >= 0:
+            if nums[left] < nums[left+1]:
+                break
+            else:
+                left -= 1
+        if left >= 0:
+            while right >= 0:
+                if nums[right] > nums[left]:
+                    nums[left], nums[right] = nums[right], nums[left]
+                    break
+                else:
+                    right -= 1
+        left = left + 1
+        right = len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
+'''
+    This is my personal record of solving Leetcode Problems. 
+    If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+    Copyright (C) 2020  mengxinayan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
