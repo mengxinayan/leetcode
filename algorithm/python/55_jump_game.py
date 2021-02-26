27ee4231f9446c547d0c84c8ffcdf69cad13a9d2
mengxinyan <yanhou529@gmail.com>
2021-02-25 23:23:04 -0500

    Add #55 Jump Game

---
+++ algorithm/python/55_jump_game.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_position = 0
+        length = len(nums)
+        for i in range(length):
+            if i <= max_position:
+                max_position = max(max_position, i + nums[i])
+                if max_position >= length - 1:
+                    return True
+        return False
+
+'''
+    This is my personal record of solving Leetcode Problems. 
+    If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+    Copyright (C) 2020  mengxinayan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
