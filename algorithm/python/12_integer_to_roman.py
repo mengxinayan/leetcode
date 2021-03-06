e3f8846a1ddd8a7c2afc76075d1ac308282d5cbf
mengxinyan <yanhou529@gmail.com>
2021-03-06 01:17:22 -0500

    Update #12 Integer to Roman

--- algorithm/python/12_integer_to_roman.py
+++ algorithm/python/12_integer_to_roman.py
@@ -2,14 +2,14 @@ class Solution:
     def intToRoman(self, num: int) -> str:
         num_ch = [[1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'], [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']]
         i = 0
-        res = ''
+        ans = ''
         while num != 0:
             if num >= num_ch[i][0]:
-                res += num_ch[i][1]
+                ans += num_ch[i][1]
                 num -= num_ch[i][0]
             else:
                 i += 1
-        return res
+        return ans
 
 '''
     This is my personal record of solving Leetcode Problems. 
