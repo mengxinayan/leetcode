ccab0ebd5545131345dcc81cdecd1efc019455a0
mengxinyan <yanhou529@gmail.com>
2021-03-06 01:17:46 -0500

    Update #13 Roman to Integer

--- algorithm/python/13_roman_to_integer.py
+++ algorithm/python/13_roman_to_integer.py
@@ -1,45 +1,45 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        res = 0
+        ans = 0
         i = 0
         while (i != len(s)):
             if s[i] == 'I':
                 if i+1 != len(s) and s[i+1] == 'V':
-                    res += 4
+                    ans += 4
                     i += 1
                 elif i+1 != len(s) and s[i+1] == 'X':
-                    res += 9
+                    ans += 9
                     i += 1
                 else:
-                    res += 1
+                    ans += 1
             elif s[i] == 'V':
-                res += 5
+                ans += 5
             elif s[i] == 'X':
                 if i+1 != len(s) and s[i+1] == 'L':
-                    res += 40
+                    ans += 40
                     i += 1
                 elif i+1 != len(s) and s[i+1] == 'C':
-                    res += 90
+                    ans += 90
                     i += 1
                 else:
-                    res += 10
+                    ans += 10
             elif s[i] == 'L':
-                res += 50
+                ans += 50
             elif s[i] == 'C':
                 if i+1 != len(s) and s[i+1] == 'D':
-                    res += 400
+                    ans += 400
                     i += 1
                 elif i+1 != len(s) and s[i+1] == 'M':
-                    res += 900
+                    ans += 900
                     i += 1
                 else:
-                    res += 100
+                    ans += 100
             elif s[i] == 'D':
-                res += 500
+                ans += 500
             else:
-                res += 1000
+                ans += 1000
             i += 1
-        return res
+        return ans
 
 '''
     This is my personal record of solving Leetcode Problems. 
