a9f48485ec8e0dbdbc64a3e4ec322c16dec8dbaa
mengxinyan <yanhou529@gmail.com>
2021-03-08 00:15:32 -0500

    Update #69 Sqrt(x)

--- algorithm/python/69_Sqrt(x).py
+++ algorithm/python/69_Sqrt(x).py
@@ -5,32 +5,32 @@ class Solution:
         elif x == 1 or x == 2 or x == 3:
             return 1
         else:
-            res = 0
+            ans = 0
             left = 0
             right = x
             mid = (left + right) // 2
             while (1):
                 if mid * mid == x:
-                    res = mid
+                    ans = mid
                     break
                 elif mid * mid > x:
                     if (mid-1) * (mid-1) <= x:
-                        res = mid - 1
+                        ans = mid - 1
                         break
                     else:
                         right = mid - 1
                         mid = (left + right) // 2
                 else:
                     if (mid+1) * (mid+1) > x:
-                        res = mid
+                        ans = mid
                         break
                     elif (mid+1) * (mid+1) == x:
-                        res = mid + 1
+                        ans = mid + 1
                         break
                     else:
                         left = mid + 1
                         mid = (left + right) // 2
-            return res
+            return ans
 
 '''
     This is my personal record of solving Leetcode Problems. 
