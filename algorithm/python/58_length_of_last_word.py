fa3813ad305d7df55b5210c14f3379907b2860a5
mengxinyan <yanhou529@gmail.com>
2021-03-08 00:12:41 -0500

    Update #58 Length of Last Word

--- algorithm/python/58_length_of_last_word.py
+++ algorithm/python/58_length_of_last_word.py
@@ -2,7 +2,7 @@ class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         if s == '': # 第一次漏掉了这种情况
             return 0
-        res = 0
+        ans = 0
         index = 0
         for i in range(len(s)-1, -1, -1):
             if s[i] != ' ':
@@ -10,10 +10,10 @@ class Solution:
                 break
         for i in range(index, -1, -1):
             if s[i] != ' ':
-                res += 1
+                ans += 1
             else:
                 break
-        return res
+        return ans
 
 '''
     This is my personal record of solving Leetcode Problems. 
