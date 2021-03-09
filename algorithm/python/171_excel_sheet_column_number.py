6af6ad7f2a62c0bbf95b003d600d8121d210e872
mengxinyan <yanhou529@gmail.com>
2021-03-09 07:17:03 -0500

    Update #171 Excel Sheet Column Number

--- algorithm/python/171_excel_sheet_column_number.py
+++ algorithm/python/171_excel_sheet_column_number.py
@@ -1,9 +1,9 @@
 class Solution:
     def titleToNumber(self, s: str) -> int:
-        res = 0
+        ans = 0
         for i in range(len(s)):
-            res = res * 26 + ord(s[i]) - ord('A') + 1
-        return res
+            ans = ans * 26 + ord(s[i]) - ord('A') + 1
+        return ans
 '''
     This is my personal record of solving Leetcode Problems. 
     If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
