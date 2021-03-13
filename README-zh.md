d9dd10e4033c68d090677110c7fa101b02a49eaa
mengxinyan <yanhou529@gmail.com>
2021-03-13 00:47:50 -0500

    Add #77 Combinations

--- README-zh.md
+++ README-zh.md
@@ -359,6 +359,7 @@
 | 100 | [Same Tree](https://leetcode-cn.com/problems/same-tree/) | [Python3](./algorithm/python/100_same_tree.py) | 简单 | 树, 深度优先搜索 |
 | 88 | [Merge Sorted Array](https://leetcode-cn.com/problems/merge-sorted-array/) | [Python3](./algorithm/python/88_merge_sorted_array.py) | 简单 | 数组, 双指针 |
 | 83 | [Remove Duplicates from Sorted List](https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/) | [Python3](./algorithm/python/83_remove_duplicates_from_sorted_list.py) | 简单 | 链表 |
+| 77 | [Combinations](https://leetcode-cn.com/problems/combinations/) | [Python3](./algorithm/python/77_combinations.py) | 中等 | 回溯算法 |
 | 75 | [Sort Colors](https://leetcode-cn.com/problems/sort-colors/) | [Python3](./algorithm/python/75_sort_colors.py) | 中等 | 排序, 数组, 双指针 |
 | 74 | [Search a 2D Matrix](https://leetcode-cn.com/problems/search-a-2d-matrix/) | [Python3](./algorithm/python/78_search_a_2D_matrix.py) | 中等 | 数组, 二分查找 |
 | 73 | [Set Matrix Zeroes](https://leetcode-cn.com/problems/set-matrix-zeroes/) | [Python3](./algorithm/python/73_set_matrix_zeroes.py) | 中等 | 数组 |
---
+++ algorithm/python/77_combinations.py
@@ -0,0 +1,32 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+
+        def backtracking(begin: int, tmp: List[int]):
+            if len(tmp) == k:
+                ans.append(tmp)
+            else:
+                for i in range(begin, n+1):
+                    backtracking(i+1, tmp+[i])
+
+        ans = []
+        backtracking(1, [])
+        return ans
+
+'''
+    This is my personal record of solving Leetcode Problems. 
+    If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+    Copyright (C) 2020  mengxinayan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
