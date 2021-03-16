a68a9661657220f22e78bbd9541f292ba28ab172
mengxinyan <yanhou529@gmail.com>
2021-03-16 00:27:33 -0400

    Update #812 Largest Triangle Area

--- algorithm/python/812_largest_triangle_area.py
+++ algorithm/python/812_largest_triangle_area.py
@@ -2,12 +2,12 @@ class Solution:
     def largestTriangleArea(self, points: List[List[int]]) -> float:
         # 使用鞋带公式
         # use Shoelace formula method
-        res=0
+        ans=0
         for i in points:
             for j in points:
                 for k in points:
-                    res=max(res,abs(i[0]*j[1]+j[0]*k[1]+k[0]*i[1]-i[1]*j[0]-j[1]*k[0]-k[1]*i[0]))
-        return res/2
+                    ans=max(ans,abs(i[0]*j[1]+j[0]*k[1]+k[0]*i[1]-i[1]*j[0]-j[1]*k[0]-k[1]*i[0]))
+        return ans/2
 
 '''
     This is my personal record of solving Leetcode Problems. 
