1f6cbe6c3024e90cf851135c208924f7747f84a5
mengxinyan <yanhou529@gmail.com>
2021-03-17 00:15:07 -0400

    Update #944 Delete Columns to Make Sorted

--- algorithm/python/944_delete_columns_to_make_sorted.py
+++ algorithm/python/944_delete_columns_to_make_sorted.py
@@ -1,12 +1,12 @@
 class Solution:
     def minDeletionSize(self, A: List[str]) -> int:
-        res = 0
+        ans = 0
         for i in range(len(A[0])):
             for j in range(len(A)-1):
                 if A[j][i] > A[j+1][i]:
-                    res += 1
+                    ans += 1
                     break
-        return res
+        return ans
 
 '''
     This is my personal record of solving Leetcode Problems. 
