20bb0f2ed4aac753f2a62aa5e0258368591e1ada
mengxinyan <yanhou529@gmail.com>
2021-03-17 00:09:43 -0400

    Update #867 Transpose Matrix

--- algorithm/python/867_transpose_matrix.py
+++ algorithm/python/867_transpose_matrix.py
@@ -1,10 +1,10 @@
 class Solution:
     def transpose(self, A: List[List[int]]) -> List[List[int]]:
-        res = [[0 for i in range(len(A))] for j in range(len(A[0]))]
+        ans = [[0 for i in range(len(A))] for j in range(len(A[0]))]
         for i in range(len(A)):
             for j in range(len(A[0])):
-                res[j][i] = A[i][j]
-        return res
+                ans[j][i] = A[i][j]
+        return ans
 
 '''
     This is my personal record of solving Leetcode Problems. 
