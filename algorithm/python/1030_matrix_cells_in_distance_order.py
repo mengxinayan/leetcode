982fea41d5ee1385b337c5ee50046ba5be0d9714
mengxinyan <yanhou529@gmail.com>
2021-03-17 23:44:09 -0400

    Update #1030 Matrix Cells in Distance Order

--- algorithm/python/1030_matrix_cells_in_distance_order.py
+++ algorithm/python/1030_matrix_cells_in_distance_order.py
@@ -1,11 +1,11 @@
 class Solution:
     def allCellsDistOrder(self, R: int, C: int, r0: int, c0: int) -> List[List[int]]:
-        res = []
+        ans = []
         for i in range(R):
             for j in range(C):
-                res.append([i, j])
-        res.sort(key = lambda x: abs(x[0]-r0) + abs(x[1]-c0) )
-        return res
+                ans.append([i, j])
+        ans.sort(key = lambda x: abs(x[0]-r0) + abs(x[1]-c0) )
+        return ans
 
 '''
     This is my personal record of solving Leetcode Problems. 
