aa233c637517adcb6a98bca97e91daf8f4654050
mengxinyan <yanhou529@gmail.com>
2021-03-21 03:05:59 -0400

    Update #1317 Convert Integer to the Sum of Two No-Zero Integers

--- algorithm/python/1317_convert_integer_to_the_sum_of_two_np-zero_integers.py
+++ algorithm/python/1317_convert_integer_to_the_sum_of_two_np-zero_integers.py
@@ -1,12 +1,12 @@
 class Solution:
     def getNoZeroIntegers(self, n: int) -> List[int]:
-        res = []
+        ans = []
         for i in range(1, n//2+1):
             if ('0' not in str(i)) and ('0' not in str(n-i)):
-                res.append(i)
-                res.append(n-i)
+                ans.append(i)
+                ans.append(n-i)
                 break
-        return res
+        return ans
 
 '''
     This is my personal record of solving Leetcode Problems. 
