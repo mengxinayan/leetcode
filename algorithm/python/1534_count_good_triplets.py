ca525afb850eb2ce98a9d13c41782d218adc9f76
mengxinyan <yanhou529@gmail.com>
2021-03-22 03:42:53 -0400

    Update #1534 Count Good Triplets

--- algorithm/python/1534_count_good_triplets.py
+++ algorithm/python/1534_count_good_triplets.py
@@ -1,12 +1,12 @@
 class Solution:
     def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
-        res = 0
+        ans = 0
         for i in range(len(arr)-2):
             for j in range(i+1, len(arr)-1):
                 for k in range(j+1, len(arr)):
                     if (abs(arr[i]-arr[j]) <= a) and (abs(arr[j]-arr[k]) <= b) and (abs(arr[i]-arr[k]) <= c):
-                        res += 1
-        return res
+                        ans += 1
+        return ans
 
 '''
     This is my personal record of solving Leetcode Problems. 
