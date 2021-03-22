8e0354ed955e0ca503d98296f8e3ea601a7d8d34
mengxinyan <yanhou529@gmail.com>
2021-03-22 03:40:03 -0400

    Update #1475 Final Prices With a Special Discount in a Shop

--- algorithm/python/1475_final_prices_with_a_special_discount_in_a_shop.py
+++ algorithm/python/1475_final_prices_with_a_special_discount_in_a_shop.py
@@ -1,14 +1,14 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        res = []
+        ans = []
         for i in range(len(prices)):
             for j in range(i+1, len(prices)):
                 if prices[j] <= prices[i]:
-                    res.append(prices[i] - prices[j])
+                    ans.append(prices[i] - prices[j])
                     break
             else:
-                res.append(prices[i])
-        return res
+                ans.append(prices[i])
+        return ans
 
 '''
     This is my personal record of solving Leetcode Problems. 
