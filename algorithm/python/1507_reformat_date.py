c6e51d2d08f8c53ad90bad0323e538fa29056eb7
mengxinyan <yanhou529@gmail.com>
2021-03-22 03:41:50 -0400

    Update #1507 Reformat Date

--- algorithm/python/1507_reformat_date.py
+++ algorithm/python/1507_reformat_date.py
@@ -9,8 +9,8 @@ class Solution:
         month_dict = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06', 'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
         month = month_dict[dates[1]]
         year = dates[2]
-        res = year + '-' + month + '-' + day
-        return res
+        ans = year + '-' + month + '-' + day
+        return ans
 
 '''
     This is my personal record of solving Leetcode Problems. 
