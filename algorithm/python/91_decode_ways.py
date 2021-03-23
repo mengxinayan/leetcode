7bd59146de3855beff0f311d936fc1a1b0e9bb19
mengxinyan <yanhou529@gmail.com>
2021-03-23 08:14:06 -0400

    Add #91 Decode Ways

---
+++ algorithm/python/91_decode_ways.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s[0] == '0':
+            return 0
+        dp = [1 for i in range(len(s)+1)]
+        for i in range(1, len(s)):
+            if int(s[i]) != 0:
+                dp[i+1] = dp[i]
+            else:
+                dp[i+1] = 0
+            if (i != 0) and (10 <= int(s[i-1:i+1]) <= 26):
+                dp[i+1] += dp[i-1]
+        return dp[-1]
+
+'''
+    This is my personal record of solving Leetcode Problems. 
+    If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+    Copyright (C) 2020  mengxinayan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
