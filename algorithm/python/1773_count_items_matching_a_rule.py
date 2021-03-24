242f4f53577c7185f32fd04577add10ec70cd435
mengxinyan <yanhou529@gmail.com>
2021-03-24 05:06:06 -0400

    Update #1773 Count Items Matching a Rule

--- algorithm/python/1773_count_items_matching_a_rule.py
+++ algorithm/python/1773_count_items_matching_a_rule.py
@@ -1,19 +1,19 @@
 class Solution:
     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
-        res = 0
+        ans = 0
         if ruleKey == 'type':
             for i in range(len(items)):
                 if items[i][0] == ruleValue:
-                    res += 1
+                    ans += 1
         elif ruleKey == 'color':
             for i in range(len(items)):
                 if items[i][1] == ruleValue:
-                    res += 1
+                    ans += 1
         else:
             for i in range(len(items)):
                 if items[i][2] == ruleValue:
-                    res += 1
-        return res
+                    ans += 1
+        return ans
 
 '''
     This is my personal record of solving Leetcode Problems. 
