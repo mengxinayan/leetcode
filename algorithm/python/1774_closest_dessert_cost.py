cd107ee943629acbac6f4d1b299528ea3890ab9a
mengxinyan <yanhou529@gmail.com>
2021-03-24 05:06:23 -0400

    Update #1774 Closest Dessert Cost

--- algorithm/python/1774_closest_dessert_cost.py
+++ algorithm/python/1774_closest_dessert_cost.py
@@ -1,18 +1,18 @@
 class Solution:
     def closestCost(self, baseCosts: List[int], toppingCosts: List[int], target: int) -> int:
         def backtracking(cost: int, begin: int):
-            if (abs(cost - target) < abs(self.res - target)) or ((abs(cost - target) == abs(self.res - target)) and (cost < self.res)):
-                self.res = cost
+            if (abs(cost - target) < abs(self.ans - target)) or ((abs(cost - target) == abs(self.ans - target)) and (cost < self.ans)):
+                self.ans = cost
             if (cost > target) or (begin == len(toppingCosts)):
                 return
             backtracking(cost + 0 * toppingCosts[begin], begin+1)
             backtracking(cost + 1 * toppingCosts[begin], begin+1)
             backtracking(cost + 2 * toppingCosts[begin], begin+1)
         
-        self.res = float('inf')
+        self.ans = float('inf')
         for cost in baseCosts:
             backtracking(cost, 0)
-        return self.res
+        return self.ans
 
 '''
     This is my personal record of solving Leetcode Problems. 
