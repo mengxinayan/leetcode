cde3cbb0620f45cdaa5ea8b4d0e0f07674817ece
mengxinyan <yanhou529@gmail.com>
2021-05-14 02:52:02 -0400

    Add #146 LRU Cache

---
+++ algorithm/python/146_LRU_cache.py
@@ -0,0 +1,56 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.queue = []
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.update_queue(key)
+            return self.cache[key]
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.update_queue(key)
+            self.cache[key] = value
+        else:
+            if len(self.queue) == self.capacity:
+                tmp = self.queue.pop()
+                self.queue.insert(0, key)
+                self.cache.pop(tmp)
+                self.cache[key] = value
+            else:
+                self.cache[key] = value
+                self.queue.insert(0, key)
+
+    def update_queue(self, key: int):
+        self.queue.remove(key)
+        self.queue.insert(0, key)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+'''
+    This is my personal record of solving Leetcode Problems. 
+    If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+    Copyright (C) 2020  mengxinayan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
