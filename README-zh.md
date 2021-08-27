c37572b048bc2be58559fc06816267d1e3d97bf7
mengxinayan <yanhou529@gmail.com>
2021-08-27 11:14:57 +0800

    Add #387 First Unique CHaracter in a String Java solution

--- README-zh.md
+++ README-zh.md
@@ -298,7 +298,7 @@
 | 401 | [Binary Watch](https://leetcode-cn.com/problems/binary-watch/) | [Python3](./algorithm/python/401_binary_watch.py) | 简单 | 位运算, 回溯算法 |
 | 392 | [Is Subsequence](https://leetcode-cn.com/problems/is-subsequence/) | [Python3](./algorithm/python/392_is_subsequence.py) | 简单 | 贪心算法, 二分查找, 动态规划 |
 | 389 | [Find the Difference](https://leetcode-cn.com/problems/find-the-difference/) | [Python3](./algorithm/python/389_find_the_difference.py) | 简单 | 位运算, 哈希表 |
-| 387 | [First Unique CHaracter in a String](https://leetcode-cn.com/problems/first-unique-character-in-a-string/) | [Python3](./algorithm/python/387_first_unique_character_in_a_string.py) | 简单 | 哈希表, 字符串 |
+| 387 | [First Unique CHaracter in a String](https://leetcode-cn.com/problems/first-unique-character-in-a-string/) | [Python3](./algorithm/python/387_first_unique_character_in_a_string.py), [Java](./algorithm/java/387_first_unique_character_in_a_string.java) | 简单 | 哈希表, 字符串 |
 | 383 | [Ransom Note](https://leetcode-cn.com/problems/ransom-note/) | [Python3](./algorithm/python/383_ransom_note.py) | 简单 | 字符串 |
 | 374 | [Guess Number Higher or Lower](https://leetcode-cn.com/problems/guess-number-higher-or-lower/) | [Python3](./algorithm/python/374_guess_number_higher_of_lower.py) | 简单 | 二分查找 |
 | 371 | [Sum of Two Integers](https://leetcode-cn.com/problems/sum-of-two-integers/) | [Python3](./algorithm/python/371_sum_of_two_integers.py) | 简单 | 位运算 |
---
+++ algorithm/java/387_first_unique_character_in_a_string.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> nonRepeatingChIndexMap = new HashMap<Character, Integer>();
+        HashSet<Character> repeatingChSet = new HashSet<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            Character ch = s.charAt(i);
+            if (repeatingChSet.contains(ch) == false) {
+                if (nonRepeatingChIndexMap.containsKey(ch) == false) {
+                    nonRepeatingChIndexMap.put(ch, i);
+                } else {
+                    nonRepeatingChIndexMap.remove(ch);
+                    repeatingChSet.add(ch);
+                }
+            }
+        }
+        if (nonRepeatingChIndexMap.values().isEmpty() == true) {
+            return -1;
+        } else {
+            return Collections.min(nonRepeatingChIndexMap.values());
+        }
+    }
+}
+
+/**
+ * This is my personal record of solving Leetcode Problems. 
+ * If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+ * Copyright (C) 2020-2021  mengxinayan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
