f738886e3f1f2a3080989101896e959b9d685ce3
mengxinayan <yanhou529@gmail.com>
2021-09-13 18:23:05 +0800

    Add #278 First Bad Version Java Solution

---
+++ algorithm/java/278_first_bad_version.java
@@ -0,0 +1,48 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        int ans = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid) == true) {
+                if (isBadVersion(mid-1) == false) {
+                    ans = mid;
+                    break;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                if (isBadVersion(mid+1) == true) {
+                    ans = mid + 1;
+                    break;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+/**
+ * This is my personal record of solving Leetcode Problems. 
+ * If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+ * Copyright (C) 2020-2021  mengxinayan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
