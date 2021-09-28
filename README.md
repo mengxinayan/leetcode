efcc68feb176dc28831fec2f385d6b3869de8a3d
mengxinayan <yanhou529@gmail.com>
2021-09-28 10:48:52 +0800

    Add #66 Plus One Java Solution

--- README.md
+++ README.md
@@ -421,7 +421,7 @@ If you have any questions or advices, please discuss them in [Issues](https://gi
 | 70 | [Climbing Stairs](https://leetcode.com/problems/climbing-stairs/) | [Python3](./algorithm/python/70_climbing_stairs.py), [Java](./algorithm/java/70_climbing_stairs.java) | Easy | Dynamic Programming |
 | 69 | [Sqrt(x)](https://leetcode.com/problems/sqrtx/) | [Python3](./algorithm/python/69_Sqrt(x).py) | Easy | Math, Binary Search |
 | 67 | [Add Binary](https://leetcode.com/problems/add-binary/) | [Python3](./algorithm/python/67_add_binary.py) | Easy | Math, String |
-| 66 | [Plus One](https://leetcode.com/problems/plus-one/) | [Python3](./algorithm/python/66_plus_one.py) | Easy | Array |
+| 66 | [Plus One](https://leetcode.com/problems/plus-one/) | [Python3](./algorithm/python/66_plus_one.py), [Java](./algorithm/java/66_plus_one.java) | Easy | Array |
 | 64 | [Minimum Path Sum](https://leetcode.com/problems/minimum-path-sum/) | [Python3](./algorithm/python/64_minimum_path_sum.py) | Medium | Array, Dynamic Programming |
 | 63 | [Unique Paths II](https://leetcode.com/problems/unique-paths-ii/) | [Python3](./algorithm/python/63_unique_paths_II.py) | Medium | Array, Dynamic Programming |
 | 62 | [Unique Paths](https://leetcode.com/problems/unique-paths/) | [Python3](./algorithm/python/62_unique_paths.py) | Medium | Array, Dynamic Programming |
---
+++ algorithm/java/66_plus_one.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length-1; i >= 0; i--) {
+            if (i != digits.length - 1) {
+                if (digits[i+1] >= 10) {
+                    digits[i+1] = digits[i+1] - 10;
+                    digits[i] += 1;
+                } else {
+                    break;
+                }
+            } else {
+                digits[i] += 1;
+            }
+        }
+        if (digits[0] == 10) {
+            int[] ans = new int[digits.length+1];
+            ans[0] = 1;
+            ans[1] = digits[0] - 10;
+            for (int i = 2; i < digits.length; i++) {
+                ans[i] = digits[i];
+            }
+            return ans;
+        } else {
+            return digits;
+        }
+    }
+}
+
+/**
+ * This is my personal record of solving Leetcode Problems. 
+ * If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+ * Copyright (C) 2020-2021  mengxinayan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
