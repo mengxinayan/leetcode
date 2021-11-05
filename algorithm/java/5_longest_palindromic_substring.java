6d21bebc8d50b1ea7b8b20a4eba153800d84bb83
mengxinayan <yanhou529@gmail.com>
2021-11-05 15:53:32 +0800

    Add #5 Longest Palindromic Substring Java Solution

---
+++ algorithm/java/5_longest_palindromic_substring.java
@@ -0,0 +1,56 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int length = s.length();
+        if (length < 2) {
+            return s;
+        }
+
+        int maxLength = 1;
+        int begin = 0;
+        boolean[][] dp = new boolean[length][length];
+        for (int i = 0; i < length; i++) {
+            dp[i][i] = true;
+        }
+
+        for (int right = 1; right < length; right++) {
+            for (int left= 0; left < right; left++) {
+                
+                if (s.charAt(left) != s.charAt(right)) {
+                    dp[left][right] = false;
+                } else {
+                    if (right - left < 3) {
+                        dp[left][right] = true;
+                    } else {
+                        dp[left][right] = dp[left+1][right-1];
+                    }
+                }
+
+                if (dp[left][right] == true && (right - left + 1 > maxLength)) {
+                    maxLength = right - left + 1;
+                    begin = left;
+                }
+            }
+        }
+
+        return s.substring(begin, begin+maxLength);
+    }
+}
+
+/**
+ * This is my personal record of solving Leetcode Problems. 
+ * If you have any questions, please discuss them in [Issues](https://github.com/mengxinayan/leetcode/issues).
+ * Copyright (C) 2020-2021  mengxinayan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
